63c32942df129964db4a95e04545b11960984eb8
phuongquy <phuongquy05@gmail.com>
2019-04-13 20:08:41 +0700

    add project

---
+++ app/src/main/java/com/example/phuongquy/leaseLover/server/server.java
@@ -0,0 +1,10 @@
+package com.example.phuongquy.leaseLover.server;
+
+public class server {
+//    public  static  String localhost="192.168.198.2:8080";
+    public  static  String localhost="192.168.64.2:81";
+    public static  String slide="http://"+localhost+"/one_love/slide/";
+    public  static String info_nu ="http://"+localhost+"/one_love/getinfo_nu.php";
+    public  static String info_nam ="http://"+localhost+"/one_love/getinfo_nam.php";
+}
+
